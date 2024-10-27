64f986ba95f5ccb007235787921ceb0522ed421a
vinzgoldwin <vinzgoldwin@gmail.com>
2024-10-27 13:42:24 +0700

    first

---
+++ android/app/src/main/kotlin/com/example/new_tunnel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new_tunnel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import path_provider_foundation
+import wireguard_flutter
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WireguardFlutterPlugin.register(with: registry.registrar(forPlugin: "WireguardFlutterPlugin"))
+}
